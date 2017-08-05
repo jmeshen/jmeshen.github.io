4d154046d3d6aac69eab0b416e65a4ed65cec774
jmeshen <jmeshen@gmail.com>
2017-08-05 14:15:46 -0400

    refactor: DRY up updateLanding method

--- js/jme.js
+++ js/jme.js
@@ -1,116 +1,57 @@
 $(document).ready(function() {
-  // $('#fullpage').fullpage({
-  //   responsive: 700
-  // });
   today = new Date();
   var curTime = today.getHours();
-  //console.log(curTime);
   checkBrowser();
-  if (sBrowser == "Chrome" || sBrowser == "Safari") {
-    updateLandingWebkit(curTime);
-  }
-  else if (sBrowser == "Firefox") {
-    updateLandingFirefox(curTime);
-  }
-  else if (sBrowser == "IE") {
-    updateLandingIE(curTime);
-  }
-  else if (sBrowser == "Opera") {
-    updateLandingOpera(curTime);
-  }
-  $("#fn-greeting").css({'opacity': '0', 'visibility': 'visible'})
-                  .animate({opacity: 1}, 1200);
-  $("#fn-i-am").delay(1500).css({'opacity': '0', 'visibility': 'visible'})
-                  .animate({opacity: 1}, 1200);
-  // $(".arrow-down").hide().delay(3000).fadeIn('slow').delay(500).animate({bottom: "4%"});
+  updateLanding(browserPrefix, curTime);
+  $("#fn-greeting")
+    .css({'opacity': '0', 'visibility': 'visible'})
+    .animate({opacity: 1}, 1200);
+  $("#fn-i-am")
+    .delay(1500)
+    .css({'opacity': '0', 'visibility': 'visible'})
+    .animate({opacity: 1}, 1200);
 });
 
 $(".arrow-down, .arrow-up").click(function(){
   $("#landing").slideToggle('slow');
 });
 
-var sBrowser, sUsrAg = navigator.userAgent;
+var sBrowser, browserPrefix, sUsrAg = navigator.userAgent;
 function checkBrowser() {
   if (sUsrAg.indexOf("Chrome") > -1) {
-      sBrowser = "Chrome";
+    sBrowser = "Chrome";
+    browserPrefix = '-webkit-';
   } else if (sUsrAg.indexOf("Safari") > -1) {
-      sBrowser = "Safari";
+    sBrowser = "Safari";
+    browserPrefix = '-webkit-';
   } else if (sUsrAg.indexOf("Opera") > -1) {
-      sBrowser = "Opera";
+    sBrowser = "Opera";
+    browserPrefix = '-o-';
   } else if (sUsrAg.indexOf("Firefox") > -1) {
-      sBrowser = "Firefox";
+    sBrowser = "Firefox";
+    browserPrefix = '-moz-';
   } else if (sUsrAg.indexOf("MSIE") > -1) {
-      sBrowser = "IE";
-  }
-}
-function updateLandingWebkit(curTime) {
-  if (curTime < 4) {
-    $('#fn-greeting').html('well met, night owl');
-    $('#landing').css({'background': '-webkit-linear-gradient(top, rgb(0, 0, 0), rgb(38, 24, 86))'});
-  }
-  else if (curTime < 12) {
-    $('#fn-greeting').html('good morning');
-    $('#landing').css({'background': '-webkit-linear-gradient(top, rgb(180, 210, 236), rgb(255, 210, 113))'});
-  }
-  else if (curTime < 17) {
-    $('#fn-greeting').html('good afternoon');
-    $('#landing').css({'background': '-webkit-linear-gradient(top, rgb(255, 210, 113), rgb(178, 209, 231))'});
-  } else {
-    $('#fn-greeting').html('good evening');
-    $('#landing').css({'background': '-webkit-linear-gradient(top, rgb(21, 20, 20), rgb(17, 81, 121)'});
-  }
-}
-function updateLandingFirefox(curTime) {
-  if (curTime < 4) {
-    $('#fn-greeting').html('well met, night owl');
-    $('#landing').css({'background': '-moz-linear-gradient(top, rgb(0, 0, 0), rgb(38, 24, 86))'});
-  }
-  else if (curTime < 12) {
-    $('#fn-greeting').html('good morning');
-    $('#landing').css({'background': '-moz-linear-gradient(top, rgb(180, 210, 236), rgb(255, 210, 113))'});
-  }
-  else if (curTime < 17) {
-    $('#fn-greeting').html('good afternoon');
-    $('#landing').css({'background': '-moz-linear-gradient(top, rgb(255, 210, 113), rgb(178, 209, 231))'});
-  } else {
-    $('#fn-greeting').html('good evening');
-    $('#landing').css({'background': '-moz-linear-gradient(top, rgb(21, 20, 20), rgb(17, 81, 121)'});
-  }
-}
-function updateLandingIE(curTime) {
-  if (curTime < 4) {
-    $('#fn-greeting').html('well met, night owl');
-    $('#landing').css({'background': '-ms-linear-gradient(top, rgb(0, 0, 0), rgb(38, 24, 86))'});
-  }
-  else if (curTime < 12) {
-    $('#fn-greeting').html('good morning');
-    $('#landing').css({'background': '-ms-linear-gradient(top, rgb(180, 210, 236), rgb(255, 210, 113))'});
-  }
-  else if (curTime < 17) {
-    $('#fn-greeting').html('good afternoon');
-    $('#landing').css({'background': '-ms-linear-gradient(top, rgb(255, 210, 113), rgb(178, 209, 231))'});
-  } else {
-    $('#fn-greeting').html('good evening');
-    $('#landing').css({'background': '-ms-linear-gradient(top, rgb(21, 20, 20), rgb(17, 81, 121)'});
-  }
-}
-function updateLandingOpera(curTime) {
-  if (curTime < 4) {
-    $('#fn-greeting').html('well met, night owl');
-    $('#landing').css({'background': '-o-linear-gradient(top, rgb(0, 0, 0), rgb(38, 24, 86))'});
-  }
-  else if (curTime < 12) {
-    $('#fn-greeting').html('good morning');
-    $('#landing').css({'background': '-o-linear-gradient(top, rgb(180, 210, 236), rgb(255, 210, 113))'});
-  }
-  else if (curTime < 17) {
-    $('#fn-greeting').html('good afternoon');
-    $('#landing').css({'background': '-o-linear-gradient(top, rgb(255, 210, 113), rgb(178, 209, 231))'});
-  } else {
-    $('#fn-greeting').html('good evening');
-    $('#landing').css({'background': '-o-linear-gradient(top, rgb(21, 20, 20), rgb(17, 81, 121)'});
+    sBrowser = "IE";
+    browserPrefix = '-ms-';
   }
 }
 
-// TO-DO: refactor to make updateLanding* more DRY
+function updateLanding(browserPrefix, curTime) {
+  if (curTime < 4) {
+    $('#fn-greeting').html('well met, night owl');
+    $('#landing').css({'background': browserPrefix + 'linear-gradient(top, rgb(0, 0, 0), rgb(38, 24, 86))'});
+  }
+  else if (curTime < 12) {
+    $('#fn-greeting').html('good morning');
+    $('#landing').css({'background': browserPrefix + 'linear-gradient(top, rgb(180, 210, 236), rgb(255, 210, 113))'});
+  }
+  else if (curTime < 17) {
+    $('#fn-greeting').html('good afternoon');
+    $('#landing').css({'background': browserPrefix + 'linear-gradient(top, rgb(255, 210, 113), rgb(178, 209, 231))'});
+  } else {
+    $('#fn-greeting').html('good evening');
+    $('#landing').css({'background': browserPrefix + 'linear-gradient(top, rgb(21, 20, 20), rgb(17, 81, 121)'});
+  }
+}
+
 
