a7c8c29b0da7f91fef2711368b5bdd29067c7292
jmeshen <hello@jmeshen.com>
2021-04-06 16:28:43 -0400

    refactor: globalstyle indentation

--- src/App.js
+++ src/App.js
@@ -6,37 +6,37 @@ import styled, { createGlobalStyle, ThemeProvider } from 'styled-components';
 import normalize from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
-${normalize}
-h1, h2, h3, h4, h5, h6 {
-  font-family: 'Lato', sans-serif;
-}
-p {
-  line-height: 1.7;
-}
-body {
-  font-family: 'Roboto Slab', sans-serif;
-  color: ${(props) => props.theme.text};
-  background: ${(props) => props.theme.bg};
-  height: 100vh;
-  margin: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: .3s;
-  @media (max-width: 500px) {
-    align-items: baseline;
-    height: auto;
+  ${normalize}
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Lato', sans-serif;
   }
-}
-a {
-  text-decoration: none;
-  color: ${(props) => `${props.theme.accent}AA`};
-  transition: .3s;
-  &:hover {
-   color: coral; 
-   transform: translateY(-3px);
+  p {
+    line-height: 1.7;
   }
-}
+  body {
+    font-family: 'Roboto Slab', sans-serif;
+    color: ${(props) => props.theme.text};
+    background: ${(props) => props.theme.bg};
+    height: 100vh;
+    margin: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: .3s;
+    @media (max-width: 500px) {
+      align-items: baseline;
+      height: auto;
+    }
+  }
+  a {
+    text-decoration: none;
+    color: ${(props) => `${props.theme.accent}AA`};
+    transition: .3s;
+    &:hover {
+    color: coral; 
+    transform: translateY(-3px);
+    }
+  }
 `;
 
 const themeObj = {
